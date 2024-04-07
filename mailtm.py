a268bdbc28807998a7ec40d947d71a8efd54c715
Xeysh <146588488+Xeysh@users.noreply.github.com>
2024-04-08 01:20:27 +0300

    Update mailtm.py

--- mailtm.py
+++ mailtm.py
@@ -2,8 +2,8 @@ import random
 import random_strings
 import requests
 
-class MailTmApi:
 
+class MailTmApi:
     def __init__(self) -> None:
         self.session = requests.session()
         self.session.headers = {
@@ -21,15 +21,22 @@ class MailTmApi:
             'sec-fetch-mode': 'cors',
             'sec-fetch-site': 'same-site',
             'sec-gpc': '1',
-            'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36',
+            'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 '
+                          '(KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36',
         }
 
     def get_random_avaible_domain(self) -> str:
+        """
+        **Random olarak bir domain alır.**
+        """
         response = self.session.get('https://api.mail.gw/domains')
         random_mail_domain = random.choice(response.json()["hydra:member"])["domain"]
         return random_mail_domain
 
-    def get_random_mail(self,domain: str) -> dict[str, str]:
+    def get_random_mail(self, domain: str) -> dict[str, str]:
+        """
+        **Random olarak bir mail alır ve şifresini, tokenini alır.**
+        """
         nickname = random_strings.random_hex(10)
         password = random_strings.random_hex(8)
 
@@ -46,7 +53,10 @@ class MailTmApi:
         response = self.session.post('https://api.mail.gw/token', json=json_data)
         return {"email": f'{nickname}@{domain}', "password": password, "token": response.json()["token"]}
 
-    def get_emails(self,mail_token) -> list[str]:
+    def get_emails(self, mail_token) -> list[str]:
+        """
+        **Random olarak alınan mail'in mail kutusuna bakar.**
+        """
         self.session.headers['authorization'] = f'Bearer {mail_token}'
         response = self.session.get('https://api.mail.gw/messages')
         return response.json()["hydra:member"]
@@ -56,3 +66,4 @@ class MailTmApi:
 #     domain = mail.get_random_avaible_domain()
 #     random_mail = mail.get_random_mail(domain)["token"]
 #     get_emails = mail.get_emails(random_mail)
+#     print(get_emails)
