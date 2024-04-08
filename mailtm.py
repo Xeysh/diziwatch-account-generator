243d6e4c42063334758751fe48dd1c6f19965959
Xeysh <146588488+Xeysh@users.noreply.github.com>
2024-04-08 18:05:53 +0300

    Update mailtm.py

--- mailtm.py
+++ mailtm.py
@@ -21,8 +21,7 @@ class MailTmApi:
             'sec-fetch-mode': 'cors',
             'sec-fetch-site': 'same-site',
             'sec-gpc': '1',
-            'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 '
-                          '(KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36',
+            'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36',
         }
 
     def get_random_avaible_domain(self) -> str:
