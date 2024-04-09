ed2ab30ea33fa2fdf4f3ed5d4277d205b5047e01
Xeysh <146588488+Xeysh@users.noreply.github.com>
2024-04-09 18:58:02 +0300

    Delete Solution_Captcha directory

--- Solution_Captcha/hekt/setup.js
+++
@@ -1,1 +0,0 @@
-!function(){function e(){browser.permissions.request({origins:["*://hekt.akmal.dev/*","*://*.hcaptcha.com/captcha/*"]})}document.getElementById("setup-button").addEventListener("click",e)}();
