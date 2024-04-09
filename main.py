ba0c6d8748d5fd4c61d7c4e5930f602f4d1d6e46
Xeysh <146588488+Xeysh@users.noreply.github.com>
2024-04-09 18:59:08 +0300

    Update main.py

--- main.py
+++ main.py
@@ -17,13 +17,6 @@ class DiziWatch:
         self.session.proxies = {'http': 'http://' + proxy.strip()}
 
     def generator(self) -> str:
-        try:
-            captcha = random.choice(open("captcha.txt", "r").readlines()).strip()
-            if not captcha:
-                raise ValueError(Fore.RED + "Captcha token boş, lütfen captcha.py'ı çalıştırın.")
-        except FileNotFoundError:
-            raise Exception(Fore.RED + "Captcha file is missing.")
-
         email = MailTmApi().get_random_mail(MailTmApi().get_random_avaible_domain())
 
         info = {
@@ -32,7 +25,7 @@ class DiziWatch:
                        'mail_id': email["email"],
                        'passwrd': "whysoserius1",
                        'passwrd2': "whysoserius1",
-                       'captcha': captcha
+                       'captcha': "03AFcWeA5M_4H4iaRIte9tyPg8P9G6He7r2SbtCMD1KIfZSS70JxpOuxjTpbIDzHPqg0p-4ofOwYOOqBY0S2QVIgV9T6GKXj8w5rAUGwWUZFFid0dZT6iwbkGRn2y5O8DI-24G5onc6mUxaDeIrlB7OJYYoz242ScF9JB3rbZBanKazuGd7VtNP1k5KPu9uOu84B-rgBui8enEWpKYcxy61FylFmtE2gBGWep2AjUYKhVDmJ6r9ia9o4vG_kSngp9DeMOaEzohKMjjOmqiEM6-585Bh35kQFFRXg27MP01BF6dHX64GU6hTCQRGihTdt4XvuDPvp1uiUTXEIx3jPJkv785lnX3t3Xe0jlMUafiqBRm_6XrWD7YXjBtqJPyUn536epc0soVPOBDxlFz1s8FVFNBnmZpIDf7rx9a8xGi0dedRDrKqN6B591pJcxtUiR97cvlXhKFBUocSp_Qku8vkqYQjGiRP9nhzU4PmQ9Fi2eV1FVUyCqgYw-RNd-BbSSklo0X4bkpwmBZ_4gZav1LGOsQyHWt6EE0tOz5hVHQ5DDzueuNmcMSndHjGMciHced396EgcAiusy0pE8lGAe0Hi9Ty2V-3rU1MLZ1iD1mXvQMlDo6GIXLhV0Oe3nO5z4RRrDIMTFfxzmP_ADsb1S7UvMaRSyQB1Mat9RL23BDEqk1UCkYDEmevn8"
                        }
 
         r = self.session.post("https://diziwatch.net/wp-admin/admin-ajax.php",
@@ -41,11 +34,6 @@ class DiziWatch:
 
         if r.status_code != 200:
             raise Exception(Fore.RED + f"Kayıt olunurken bir sorun oluştu. - {r.status_code}\n{r.text}")
-
-        elif r.status_code == 400:
-            with open("captcha.txt") as f:
-                f.truncate(0)
-            raise Exception(Fore.RED + f"Captcha tokeninin süresi dolmuş, lütfen captcha.py'ı çalıştırın.")
 
         return (Fore.GREEN + f"Başarıyla kayıt olundu. - {r.status_code}\n"
                              f"İsim: {info['username']}\n"
